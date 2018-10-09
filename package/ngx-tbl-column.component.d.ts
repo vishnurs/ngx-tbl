98af5edfb11038d58a3b5c9501864da7e27517c4
Vishnu <rsvishnuu@gmail.com>
2018-10-09 14:17:25 +0530

    update license

--- package/ngx-tbl-column.component.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class NgxTblColumn {
-    name: string;
-    key: string;
-    sortable: string;
-    narrow: boolean;
-    cellTemplate: any;
-}
--- package/public_api.d.ts
+++
@@ -1,5 +0,0 @@
-export { NgxTblModule } from './ngx-tbl.module';
-export * from './ngx-tbl-component';
-export { NgxTblControl } from './ngx-tbl-control';
-export { NgxTblColumn } from './ngx-tbl-column.component';
-export { NgxTblPagination } from './ngx-tbl-pagination/ngx-tbl-pagination';
